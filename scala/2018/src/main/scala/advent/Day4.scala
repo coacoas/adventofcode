42298db3301f87404289a9dc590bf514d88f6bf2
Bill Carlson <bill@coacoas.net>
2018-12-04 22:37:55 -0500

    Day 4 - Scala

---
+++ scala/2018/src/main/scala/advent/Day4.scala
@@ -0,0 +1,68 @@
+package advent
+
+object Day4 {
+  type GuardId = Int
+
+  val GuardEntry = """\[.*\] Guard \#(\d+) begins shift$""".r
+  val Sleep = """\[.*(\d\d)\] falls asleep$""".r
+  val Wake = """\[.*(\d\d)\] wakes up""".r
+
+  GuardEntry.unapplySeq("[1518-04-01 00:00] Guard #3167 begins shift")
+
+  def sleepLog(log: List[String]): Map[GuardId, Vector[Int]] = {
+    def go(
+      remaining: List[String],
+      sleep: Int,
+      events: Map[GuardId, Vector[Int]],
+      id: GuardId
+    ): Map[GuardId, Vector[Int]] =
+      if (remaining.isEmpty) events
+      else {
+        val entry = remaining.head
+        entry match {
+          case GuardEntry(id) => go(remaining.tail, -1, events, id.toInt)
+          case Sleep(minute) => go(remaining.tail, minute.toInt, events, id)
+          case Wake(minute) =>
+            if (sleep == -1) throw new RuntimeException("Something bad")
+            val minutes = (sleep until (minute.toInt)).toVector
+            val previous = events.get(id).getOrElse(Vector.empty)
+            go(remaining.tail, -1, events + (id -> (previous ++ minutes)), id)
+        }
+      }
+
+    go(log, -1, Map.empty, 0)
+  }
+
+  def sleepingGuard(log: Map[GuardId, Vector[Int]]): Int = 
+    log.mapValues(_.length).toList.sortBy(_._2).reverse.head._1
+  def sleepingMinute(log: Vector[Int]): Int =
+    log.groupBy(identity).toList.sortBy(_._2.length).reverse.head._1
+
+  def findGuardAndMinute(log: Map[GuardId, Vector[Int]]): Int = {
+    val (guard, minute) = log.toList.flatMap { case (id, minutes) =>
+      minutes.map(id -> _)
+    }.groupBy(identity)
+      .mapValues(_.length)
+      .toList
+      .sortBy(_._2)
+      .reverse
+      .head._1
+
+    guard * minute
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = io.Source.fromURL(this.getClass.getClassLoader.getResource("day4.txt"))
+    val data = input.getLines.toList.sorted
+    input.close()
+
+    val minutes = sleepLog(data)
+    val guard = sleepingGuard(minutes)
+    val minute = sleepingMinute(minutes(guard))
+    val guardMinute = findGuardAndMinute(minutes)
+
+    println(s"Result 1: ${guard * minute}")
+    println(s"Result 2: $guardMinute")
+  }
+
+}
